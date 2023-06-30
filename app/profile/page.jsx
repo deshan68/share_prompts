3e6e69164e748291d7b7e1c5ae0417c30d6b6704
deshan68 <arundeshan>
2023-06-30 08:40:48 +0530

    prompt edit

---
+++ app/profile/page.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Profile from "@components/Profile";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const MyProfile = () => {
+  const [posts, setPosts] = useState([]);
+  const { data: session } = useSession();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setPosts(data);
+    };
+
+    fetchPosts();
+  }, []);
+
+  const handleEdit = (post) => {
+    router.push(`update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {};
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default MyProfile;
